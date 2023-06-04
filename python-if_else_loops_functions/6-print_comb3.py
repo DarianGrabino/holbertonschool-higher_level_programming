a260982e60ebc04b11b4c498290c09643d3d2e34
DarianGrabino <dariangra@gmail.com>
2023-06-04 13:25:34 -0700

    change print to .format

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -5,7 +5,7 @@ for n in range(0, 10):
         if (n > i) or (n == i):
             continue
         else:
-            print(f"{n}{i}", end="")
+            print("{}{}".format(n, i), end="")
         if (n < 8):
             print(", ", end="")
 print()
