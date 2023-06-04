54e599395cf19d6a7a8037c49d909e4d8db714a4
DarianGrabino <dariangra@gmail.com>
2023-06-04 15:14:35 -0700

    prints a string in uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    result = ""
+    for n in str:
+        if ord(n) >= 92 and ord(n) <= 122:
+            result += chr(ord(n) - 32)
+        else:
+            result += n
+    print(result)
