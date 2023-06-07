ca343ac5dea6d49f149c536b187c0b8bf95727d5
DarianGrabino <dariangra@gmail.com>
2023-06-07 06:59:24 -0700

    change code

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
-#!/usr/bin/pyton3
-
+#!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for element in row:
-            print("{}".format(element), end=" ")
+        for value in row:
+            print("{:d}".format(value), end="")
+            if row[-1] != value:
+                print(" ", end="")
         print()
