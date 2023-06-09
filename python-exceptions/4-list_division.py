98e0dc3d62f57d91257f0c42b70e6fba1fadc619
DarianGrabino <dariangra@gmail.com>
2023-06-09 11:49:54 -0700

    change finally

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -14,5 +14,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("wrong type")
         finally:
-            pass
+            True
     return result
