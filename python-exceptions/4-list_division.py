d887e61b66895159ae2031998eafe2532207db6e
DarianGrabino <dariangra@gmail.com>
2023-06-09 11:53:57 -0700

    add parenthesis

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -5,15 +5,15 @@ def list_division(my_list_1, my_list_2, list_length):
     for n in range(list_length):
         try:
             result.append(my_list_1[n] / my_list_2[n])
-        except ZeroDivisionError:
+        except (ZeroDivisionError):
             print("division by 0")
             result.append(0)
         except (ValueError, TypeError):
             print("out of range")
             result.append(0)
-        except IndexError:
+        except (IndexError):
             print("wrong type")
             result.append(0)
         finally:
-            pass
+            True
     return result
