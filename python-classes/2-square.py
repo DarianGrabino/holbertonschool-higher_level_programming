15f017f1cca6d65718cbee4e0827fc32fec50229
DarianGrabino <dariangra@gmail.com>
2023-06-14 14:22:14 -0700

    add comment

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,6 +5,7 @@
 class Square:
     """Define a class Square with attribute"""
     def __init__(self, size=0):
+        """add exception"""
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
