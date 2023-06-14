bca2d35e781cccb6dedcdce1d0ebefc328aaa697
DarianGrabino <dariangra@gmail.com>
2023-06-14 16:09:33 -0700

    add TypeError

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -45,3 +45,9 @@ class Square:
     def position(self, value):
         if (type(value) is not tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
+        if (type(value[0]) != int) or (type(value[1]) != int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if (value[0] < 0) or (value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
