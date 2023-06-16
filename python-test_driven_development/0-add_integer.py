81d74e4421424d3fb55b8bceb6881948864df8b3
DarianGrabino <dariangra@gmail.com>
2023-06-16 09:56:14 -0700

    change logic because python3 -m doctest error

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,11 +4,8 @@
 
 def add_integer(a, b=98):
     """ a + b"""
-    try:
-        result = int(a) + int(b)
-    except TypeError:
-        if not isinstance(a, (int, float)):
-            raise TypeError("a must be an integer")
-        if type(b) != int and type(b) != float:
-            raise TypeError("b must be an integer")
-    return result
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
