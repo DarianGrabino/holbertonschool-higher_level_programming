8d4fbee295a3bf214f58dbb1922c6469a191c7ce
DarianGrabino <dariangra@gmail.com>
2023-06-16 09:29:14 -0700

    fix return

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -11,4 +11,4 @@ def add_integer(a, b=98):
             raise TypeError("a must be an integer")
         if type(b) !=  int and type(b) != float:
             raise TypeError("b must be an integer")
-    return result
+    return int(result)
