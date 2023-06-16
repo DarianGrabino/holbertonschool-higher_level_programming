9b4b4e04712285492e1b607ed6131d91f46b0fef
DarianGrabino <dariangra@gmail.com>
2023-06-16 09:33:51 -0700

    fix pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,9 +6,9 @@ def add_integer(a, b=98):
     """ a + b"""
     try:
         result = a + b
-    except:
+    except TypeError:
         if not isinstance(a, (int, float)):
             raise TypeError("a must be an integer")
-        if type(b) !=  int and type(b) != float:
+        if type(b) != int and type(b) != float:
             raise TypeError("b must be an integer")
     return int(result)
