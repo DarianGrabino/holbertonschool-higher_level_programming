4675217a07529672b2c05b4d405a1798a9703434
DarianGrabino <dariangra@gmail.com>
2023-06-16 09:26:26 -0700

    first test

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""add integer"""
+
+
+def add_integer(a, b=98):
+    """ a + b"""
+    try:
+        result = a + b
+    except:
+        if not isinstance(a, (int, float)):
+            raise TypeError("a must be an integer")
+        if type(b) !=  int and type(b) != float:
+            raise TypeError("b must be an integer")
+    return result
