f819049133ae10a6b952ad6fec2c36afcf505f1d
DarianGrabino <dariangra@gmail.com>
2023-06-16 11:50:24 -0700

    fix indentatiion

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -10,14 +10,13 @@ def matrix_divided(matrix, div):
             if not isinstance(element, (int, float)):
                 raise TypeError("matrix must be a matrix \
                                 (list of lists) of integers/floats")
-            if len(matrix) > 1:
-                if len(matrix[0]) != len(matrix[1]):
-                    raise TypeError("Each row of the matrix \
-                                    must have the same size")
-            if (type(div)) != int and (type(div)) != float:
-                raise TypeError("div must be a number")
-            if div == 0:
-                raise ZeroDivisionError("division by zero")
+    if len(matrix) > 1:
+        if len(matrix[0]) != len(matrix[1]):
+            raise TypeError("Each row of the matrix must have the same size")
+    if (type(div)) != int and (type(div)) != float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
     for row in matrix:
         new_row = list(map(lambda n: round(n / div, 2), row))
         new_matrix.append(new_row)
