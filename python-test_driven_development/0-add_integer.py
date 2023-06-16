13bd4f8221b521b7d0adbdfc5d4936e1e2713466
DarianGrabino <dariangra@gmail.com>
2023-06-16 09:37:53 -0700

    a and b must be first casted to integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,10 +5,10 @@
 def add_integer(a, b=98):
     """ a + b"""
     try:
-        result = a + b
+        result = int(a) + int(b)
     except TypeError:
         if not isinstance(a, (int, float)):
             raise TypeError("a must be an integer")
         if type(b) != int and type(b) != float:
             raise TypeError("b must be an integer")
-    return int(result)
+    return result
