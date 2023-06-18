264069ffed7422ec4b55279f37c1163d43ee4489
DarianGrabino <dariangra@gmail.com>
2023-06-18 09:49:13 -0700

    print square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""""print square"""
+
+
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float):
+        raise TypeError("size must be an integer")
+    else:
+        for n in range(size):
+            for i in range(size):
+                print("#", end="")
+            print()
