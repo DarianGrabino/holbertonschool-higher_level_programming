a4666e60c7529201def214bd340ff1f066013964
DarianGrabino <dariangra@gmail.com>
2023-06-22 12:08:14 -0700

    advanced

---
+++ python-everything_is_object/100-magic_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def magic_string():
+    str = "BestSchool"
+    if not hasattr(magic_string, "count"):
+        magic_string.count = 0
+    magic_string.count += 1
+    return ", ".join(["BestSchool"] * magic_string.count)
