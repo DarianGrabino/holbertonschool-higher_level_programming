56be5628153180cd382127938b22e618882834e4
DarianGrabino <dariangra@gmail.com>
2023-06-22 15:48:10 -0700

    import BaseGeometry

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """Class Square that inherits from Rectangle"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-     """initialize by validating size"""
+    """initialize by validating size"""
     def __init__(self, size):
-        self.integer_validator("size", size)
+        BaseGeometry.integer_validator(self, "size", size)
         self.__size = size
         super().__init__(size, size)
         self.__width = size
         self.__height = size
 
     def area(self):
-       """area of square"""
+        """area of square"""
         return self.__width ** 2
