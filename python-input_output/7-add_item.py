dacd1bd9744d2667717620c803f0e0ebf035009f
DarianGrabino <dariangra@gmail.com>
2023-06-26 13:55:59 -0700

    fix error

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """Load, add, save."""
-import json
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 # Cargamos los elementos existentes desde el archivo, o creamos una lista vacía
+filename = "add_item.json"
 try:
-    items = load_from_json_file("add_item.json")
+    lista = load_from_json_file(filename)
 except FileNotFoundError:
-    items = []
+    lista = []
 
 # Agregamos los nuevos elementos a la lista
 for arg in sys.argv[1:]:
-    items.append(arg)
+    lista.append(arg)
 
 # Guardamos la lista actualizada en el archivo
-save_to_json_file(items, "add_item.json")
+save_to_json_file(lista, filename)
