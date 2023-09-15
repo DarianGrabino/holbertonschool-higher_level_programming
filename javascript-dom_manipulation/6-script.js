ca66430b12fa8fb6d148a7faf9634b21eb8f0c86
DarianGrabino <dariangra@gmail.com>
2023-09-15 12:55:48 -0700

    fetches the character name from a URL

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,11 @@
+// fetches the character name from a URL
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    var characterName = data.name;
+
+    var characterElement = document.getElementById("character");
+
+    characterElement.textContent = characterName;
+  })
+  .catch(error => console.error('Error:', error));
