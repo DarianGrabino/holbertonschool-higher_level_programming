705da014e432441c19c6444e9e42881b4d61164d
DarianGrabino <dariangra@gmail.com>
2023-09-19 11:21:43 -0700

    toggles the class of the header

---
+++ javascript-web_jquery/4-script.js
@@ -0,0 +1,16 @@
+// toggles the class of the header
+$(document).ready(function ()
+{
+    $('#toggle_header').click(function ()
+    {
+        var header = $('header');
+        if (header.hasClass('red')) 
+        {
+            header.removeClass('red').addClass('green');
+        }
+        else 
+        {
+            header.removeClass('green').addClass('red');
+        }
+    });
+});
